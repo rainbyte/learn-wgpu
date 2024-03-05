83582d53770fadf47f73d51b0c51b83b486119e6
Alvaro Fernando García <alvarofernandogarcia@gmail.com>
2024-03-04 21:51:34 -0300

    Add 6th example: triangles

---
+++ wgpu05/src/main.rs
@@ -0,0 +1,29 @@
+mod common;
+
+use winit::event_loop::EventLoop;
+
+fn main() {
+    let mut primitive_type = "triangle-list";
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() > 1 {
+        primitive_type = &args[1];
+    }
+
+    let mut topology = wgpu::PrimitiveTopology::TriangleList;
+    let mut index_format = None;
+    if primitive_type == "triangle-strip" {
+        topology = wgpu::PrimitiveTopology::TriangleStrip;
+        index_format = Some(wgpu::IndexFormat::Uint32);
+    }
+
+    let inputs = common::Inputs {
+        source: wgpu::ShaderSource::Wgsl(include_str!("shader.wgsl").into()),
+        topology: topology,
+        strip_index_format: index_format,
+    };
+    let event_loop = EventLoop::new().unwrap();
+    let window = winit::window::Window::new(&event_loop).unwrap();
+    window.set_title(&*format!("{}: {}", "Primitive", primitive_type));
+
+    pollster::block_on(common::run(event_loop, &window, inputs, 9));
+}

5593af24ec827f193c87ce6953467a9134bb0792
Alvaro Fernando García <alvarofernandogarcia@gmail.com>
2024-03-04 21:40:03 -0300

    Initial commit

---
+++ wgpu01/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let instances = wgpu::Instance::new(wgpu::InstanceDescriptor::default());
+    for adapter in instances.enumerate_adapters(wgpu::Backends::all()) {
+        println!("{:?}", adapter.get_info())
+    }
+}

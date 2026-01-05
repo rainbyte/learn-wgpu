f6e728aaee121e271bc7bb33be9ac389eaf22ec2
Alvaro Fernando García <alvarofernandogarcia@gmail.com>
2026-01-05 09:27:02 -0300

    wgpu11: consume vertex data for sphere

--- wgpu11/src/main.rs
+++ wgpu11/src/main.rs
@@ -2,27 +2,25 @@ mod common;
 mod vertex_data;
 mod transforms;
 
-fn vertex(p: [i8; 3], n: [i8; 3]) -> common::Vertex {
+fn vertex(p: [f32; 3], n: [f32; 3]) -> common::Vertex {
     common::Vertex {
         position: [
-            p[0] as f32,
-            p[1] as f32,
-            p[2] as f32,
+            p[0],
+            p[1],
+            p[2],
             1.0
         ],
         normal: [
-            n[0] as f32,
-            n[1] as f32,
-            n[2] as f32,
+            n[0],
+            n[1],
+            n[2],
             1.0
         ],
     }
 }
 
-fn create_vertices() -> Vec<common::Vertex> {
-    let pos = vertex_data::cube_positions();
-    let normal = vertex_data::cube_normals();
-
+fn create_vertices(r: f32, u: usize, v: usize) -> Vec<common::Vertex> {
+    let (pos, normal, _uvs) = vertex_data::sphere_data(r, u, v);
     let mut data: Vec<common::Vertex> = Vec::with_capacity(pos.len());
     for i in 0..pos.len() {
         data.push(vertex(pos[i], normal[i]));
@@ -31,7 +29,7 @@ fn create_vertices() -> Vec<common::Vertex> {
 }
 
 fn main() {
-    let vertex_data = create_vertices();
+    let vertex_data = create_vertices(2.0, 15, 20);
     let light_data = common::light(
         [1.0, 0.0, 1.0],
         [1.0, 1.0, 0.0],
@@ -40,5 +38,5 @@ fn main() {
         0.7,
         30.0
     );
-    common::run(&vertex_data, light_data, "Cube with Lightning");
+    common::run(&vertex_data, light_data, "Sphere with Lightning");
 }
